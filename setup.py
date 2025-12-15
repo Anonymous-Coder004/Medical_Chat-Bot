a30d3efb039caed9c5bae331f0d254f7ff9049fb
Yash_Srivastava <yash13sri@gmail.com>
2025-12-15 21:45:39 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Yash Srivastava",
+    author_email="yash13sri@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
